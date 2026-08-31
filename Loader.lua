d1d70873cddc10791b8f6cd1dc448566f0e11624
kdkdirjrne <ruigie2007gabotero@gmail.com>
2026-08-31 09:11:42 +0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -2,4 +2,4 @@
 Source: https://www.mediafire.com/file/cykhfwhakr9cwb0/Open_Source_Password__42.zip/file
 Password: 42
 ]]
-loadstring(game:HttpGet("https://roblox.vgxmodhub.workers.dev/loader"))()
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/795ecd4e8dd40e81dbf3ead7d0a3099f1d39ea25d5d60b9219adcf70ddfaf998/download"))()
