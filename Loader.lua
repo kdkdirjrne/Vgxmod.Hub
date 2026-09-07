fb784f01b3ef221b12782414e3f1bfecc0c16baa
kdkdirjrne <ruigie2007gabotero@gmail.com>
2026-09-07 15:43:35 +0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,9 +1,7 @@
 --[[ 
 Source: https://www.mediafire.com/file/cykhfwhakr9cwb0/Open_Source_Password__42.zip/file
 Password: 42
+Reminder: Do not leak skidi
 ]]
 
-
-
-
-loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/3bcc0a2b7346a8bfe88b2d431526b03afd2001118730792679c2a564be0e40d1/download"))()
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/795ecd4e8dd40e81dbf3ead7d0a3099f1d39ea25d5d60b9219adcf70ddfaf998/download"))()
