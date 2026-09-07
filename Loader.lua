2ab7153c2c05c4cf2892917c2d03543fc962b22a
kdkdirjrne <ruigie2007gabotero@gmail.com>
2026-09-07 15:33:28 +0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -3,3 +3,4 @@ Source: https://www.mediafire.com/file/cykhfwhakr9cwb0/Open_Source_Password__42.
 Password: 42
 ]]
 loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/3bcc0a2b7346a8bfe88b2d431526b03afd2001118730792679c2a564be0e40d1/download"))()
+-- gay
